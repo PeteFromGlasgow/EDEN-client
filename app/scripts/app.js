c9ab054287876c8d9c6fc4b52b8c021a2f44d231
Peter Kinnaird <peter@peterkinnaird.eu>
2014-04-12 11:55:36 +0100

    Initial Commit for EDEN interface containing the Angular Frameword

---
+++ app/scripts/app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+angular.module('edenClientApp', [
+  'ngCookies',
+  'ngResource',
+  'ngSanitize',
+  'ngRoute'
+])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
