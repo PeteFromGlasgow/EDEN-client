08ff1742175c579f84595c7e3307b424ceb5e58f
Peter Kinnaird <peter@peterkinnaird.eu>
2014-04-12 14:19:35 +0100

    Add functionality too keep aspect ratio and demo canvas code.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,5 +1,46 @@
 'use strict';
 
+
+// Stolen so we dont have to care about the canvas
+function onResize(stage)
+{
+var keepAspectRatio = true;
+// browser viewport size
+var w = window.innerWidth;
+var h = window.innerHeight;
+
+// stage dimensions
+var ow = 640; // your stage width
+var oh = 480; // your stage height
+
+if (keepAspectRatio)
+{
+    // keep aspect ratio
+    var scale = Math.min(w / ow, h / oh);
+    stage.scaleX = scale;
+    stage.scaleY = scale;
+
+   // adjust canvas size
+   stage.canvas.width = ow * scale;
+  stage.canvas.height = oh * scale;
+}
+else
+{
+    // scale to exact fit
+    stage.scaleX = w / ow;
+    stage.scaleY = h / oh;
+
+    // adjust canvas size
+    stage.canvas.width = ow * stage.scaleX;
+    stage.canvas.height = oh * stage.scaleY;
+   }
+
+ // update the stage
+stage.update()
+}
+
+
+
 angular.module('edenClientApp')
   .controller('MainCtrl', function ($scope) {
     $scope.awesomeThings = [
@@ -7,4 +48,21 @@ angular.module('edenClientApp')
       'AngularJS',
       'Karma'
     ];
+
+    // Main Game controller
+    var stage = new createjs.Stage("gamecanvas");
+    var circle = new createjs.Shape();
+    circle.graphics.beginFill("red").drawCircle(0, 0, 40);
+    //Set position of Shape instance.
+    circle.x = circle.y = 50;
+    //Add Shape instance to stage display list.
+    stage.addChild(circle);
+    //Update stage will render next frame
+    stage.update();
+
+    window.onresize = function()
+	{
+	     onResize(stage);
+	}
+
   });
