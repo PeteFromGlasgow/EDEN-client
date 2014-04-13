fc9dfee738c637cf4e1781095a6427a1c7447491
Alistair McKinlay <github@10people.co.uk>
2014-04-13 01:57:08 +0100

    Added components list and dragging/dropping onto canvas.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -118,4 +118,78 @@ angular.module('edenClientApp')
 		stage.update();
 	},33);
 
-  });
+  }).directive('draggable', function() {
+    return function(scope, element) {
+        var el = element[0];
+
+        el.draggable = true;
+
+        el.addEventListener(
+            'dragstart',
+            function(e) {
+                e.dataTransfer.effectAllowed = 'move';
+                e.dataTransfer.setData("name",this.innerHTML);
+                this.classList.add('drag');
+                return false;
+            },
+            false
+        );
+
+        el.addEventListener(
+            'dragend',
+            function(e) {
+                this.classList.remove('drag');
+                return false;
+            },
+            false
+        );
+    }
+}).directive('droppable', function() {
+    return {
+        scope: {},
+        link: function(scope, element) {
+            var el = element[0];
+            
+            el.addEventListener(
+                'dragover',
+                function(e) {
+                    e.dataTransfer.dropEffect = 'move';
+                    if (e.preventDefault) e.preventDefault();
+                    this.classList.add('over');
+                    return false;
+                },
+                false
+            );
+            
+            el.addEventListener(
+                'dragenter',
+                function(e) {
+                    this.classList.add('over');
+                    return false;
+                },
+                false
+            );
+
+            el.addEventListener(
+                'dragleave',
+                function(e) {
+                    this.classList.remove('over');
+                    return false;
+                },
+                false
+            );
+            
+            el.addEventListener(
+                'drop',
+                function(e) {
+                    if (e.stopPropagation) e.stopPropagation();
+                    this.classList.remove('over');
+                    console.log("Added: " + e.dataTransfer.getData("name"));
+
+                    return false;
+                },
+                false
+            );
+        }
+    }
+});
